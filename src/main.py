1993178349131e28c060044ec4e1caa6aaa8c300
Julien Vieillevigne <64490661+julien-vv@users.noreply.github.com>
2025-04-24 03:38:16 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,28 +2,42 @@
 from preprocess_data import *
 from evaluate_model import *
 from train_model import *
+import mlflow
+import mlflow.sklearn
 
 def main():
-    # Take the path of the dataset
-    path = '..\data\creditcard.csv'
-    # Read the file
-    df = read_file(path)
-    # Preprocess the dataset
-    df = preprocessing_data(df)
-    # Split the dataset
-    X_train, X_test, y_train, y_test = prep_for_training(df)
+    with mlflow.start_run():
 
-    # Train 3 different model
-    model1 = train_LR(X_train, y_train)
-    model2 = train_RF(X_train, y_train)
-    model3 = train_XGB(X_train, y_train)
+        path = '..\data\creditcard.csv'
+        df = read_file(path)
+        df = preprocessing_data(df)
+        X_train, X_test, y_train, y_test = prep_for_training(df)
 
-    # Evaluate 3 different model
-    conf_mat1, class_rep1, score_roc_auc1, auprc1 = evaluate_model(model1, X_test, y_test)
-    conf_mat2, class_rep2, score_roc_auc2, auprc2 = evaluate_model(model2, X_test, y_test)
-    conf_mat3, class_rep3, score_roc_auc3, auprc3 = evaluate_model(model3, X_test, y_test)
+        # Train
+        model1 = train_LR(X_train, y_train)
+        model2 = train_RF(X_train, y_train)
+        model3 = train_XGB(X_train, y_train)
 
-    print("Program done")
+        # Evaluate
+        conf_mat1, class_rep1, score_roc_auc1, auprc1 = evaluate_model(model1, X_test, y_test)
+        conf_mat2, class_rep2, score_roc_auc2, auprc2 = evaluate_model(model2, X_test, y_test)
+        conf_mat3, class_rep3, score_roc_auc3, auprc3 = evaluate_model(model3, X_test, y_test)
+
+        # Log with mlflow
+        mlflow.log_metric("roc_auc_score_LR", score_roc_auc1)
+        mlflow.log_metric("average_precision_LR", auprc1)
+
+        mlflow.log_metric("roc_auc_score_RF", score_roc_auc2)
+        mlflow.log_metric("average_precision_RF", auprc2)
+
+        mlflow.log_metric("roc_auc_score_XGB", score_roc_auc3)
+        mlflow.log_metric("average_precision_XGB", auprc3)
+
+        mlflow.sklearn.log_model(model1, "LogisticRegression_model")
+        mlflow.sklearn.log_model(model2, "RandomForest_model")
+        mlflow.sklearn.log_model(model3, "XGBoost_model")
+
+        print("Program done")
 
 if __name__ == "__main__":
     main()

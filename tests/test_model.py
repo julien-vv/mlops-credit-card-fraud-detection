31ef4370e8c2cbeafe4e7a99e671757a84e8c90c
Julien Vieillevigne <64490661+julien-vv@users.noreply.github.com>
2025-06-04 21:50:49 +0200

    Add files via upload

--- tests/test_model.py
+++ tests/test_model.py
@@ -14,10 +14,17 @@ class TestTrainModel(unittest.TestCase):
         X, y = make_classification(n_samples=100, n_features=10, n_informative=5, n_classes=2, random_state=42)
         self.X_train = X
         self.y_train = y
+        self.params = {
+            'max_depth': 3,
+            'learning_rate': 0.1,
+            'n_estimators': 100,
+            'objective': 'binary:logistic',
+            'eval_metric': 'logloss'
+        }
 
     def test_train_XGB(self):
         # Train XGB
-        model_XGB = train_XGB(self.X_train, self.y_train)
+        model_XGB = train_XGB(self.X_train, self.y_train, self.params)
 
         self.assertIsInstance(model_XGB, XGBClassifier)
 
